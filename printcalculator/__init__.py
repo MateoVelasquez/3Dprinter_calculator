2237b18b4031bf47448d5a861c80779e9397bc60
MateoVelasquez <mateo10velasquez@hotmail.com>
2021-04-01 18:10:07 -0500

    Archivo init

---
+++ printcalculator/__init__.py
@@ -0,0 +1,9 @@
+__author__ = "Mateo Velásquez"
+__copyright__ = "2021, Smart&Printing 3D"
+__credits__ = [
+    "Mateo Velásquez", "Margarita Rosa Velásquez"
+]
+__license__ = "Propietary"
+__version__ = "Alpha"
+__maintainer__ = "Mateo Velásquez"
+__email__ = "mateo10velasquez@hotmail.com"
